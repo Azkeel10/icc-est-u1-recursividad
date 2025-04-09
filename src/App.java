fc52e88b617b5b7e02965bfc24fa3ff1fc1e1b75
Jaime <Jismalt26@gmail.com>
2025-04-09 16:13:28 -0500

    primera parte

---
+++ src/App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("---------Programa funcionando----------");
+    
+        int n = 5;
+        int resultado = factorial(n);
+    
+        System.out.println("Resultado: " + resultado);
+    }
+    
+    public static int factorial(int n) {
+        if (n == 0) {
+            System.out.println("Alcancé el caso base");
+            return 1;
+        }
+    
+        int resultado = n * factorial(n - 1);
+        System.out.println("Calculando factorial de: " + n + " * factorial(" + (n - 1) + ") " + "= " + resultado);
+    
+        return resultado;
+    }
+}
+    
